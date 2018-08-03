b255f958ff10abbc5a18286abb4fd38dd2708a96
Phi Xuan Hoan <phixuanhoan@Phis-MBP.net.fpt>
2018-08-03 08:46:40 +0700

    Add webPXH

---
+++ php/field.php
@@ -0,0 +1,82 @@
+<div class="container">
+    <div class="row title">
+        <h2>What we do ?</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sequi dicta non voluptate aspernatur ex beatae pariatur exercitationem est architecto, tempore libero recusandae et tenetur repellat nesciunt eligendi vitae aliquid?</p>
+    </div>
+    <div class="row">
+        <!-- 
+            <i class="fas fa-link"></i>
+            <i class="fas fa-cogs"></i>
+            <i class="fas fa-wrench"></i>
+            <i class="fas fa-volume-up"></i>
+            <i class="fas fa-university"></i>
+            <button type="button" class="btn btn-default" data-container="body"
+				data-toggle="popover" data-placement="top"
+				data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+				Popover on top
+			</button>
+     -->
+        <div class="col-md-2">
+            <div class="well well-small">
+                <h4  data-toggle="popover" data-placement="top"
+                data-content="> Bootstrap <br />> jQuery <br />> Javascript <br />> HTML - CSS"
+                data-original-title="Web design represents You ..." >
+					<i class="fa fa-magic"></i>
+                    <span class="">Design</span>
+                </h4>
+                <a href="#">View detail</a>
+                <div class="popover2-title" style="display: none;"><h3>ABC</h3></div>
+                <ul class="popover2-content" style="display: none;">
+                    <li>123</li>
+                    <li>456</li>
+                    <li>789</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well well-small" >
+                <h4 id="popover-3" >
+                    <i class="fa fa-link"></i>					
+                    <span class="">SEO</span>
+                </h4>
+                <a href="#">View detail</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well well-small">
+                <h4>
+                <i class="fa fa-cogs"></i>                    
+                    <span class="">Development</span>
+                </h4>
+                <a href="#">View detail</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well well-small">
+                <h4>
+                    <i class="fa fa-wrench"></i>                    
+                    <span class="">Optimiations</span>
+                </h4>
+                <a href="#">View detail</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well well-small">
+                <h4>
+                    <i class="fa fa-bullhorn"></i>
+                    <span class="">Customizations</span>
+                </h4>
+                <a href="#">View detail</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="well well-small">
+                <h4>
+                    <i class="fa fa-code"></i>
+                    <span class="">Open-source</span>
+                </h4>
+                <a href="#">View detail</a>
+            </div>
+        </div>
+    </div>
+</div>
